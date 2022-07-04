20137060bf5b7a25d3a8d82fb783598eeb38b4d5
BuildTools <unconfigured@null.spigotmc.org>
2022-07-04 14:10:11 +0900

    class

--- buildSrc/src/main/kotlin/Dependency.kt
+++ buildSrc/src/main/kotlin/Dependency.kt
@@ -1,5 +1,5 @@
 object Dependency {
     object Kotlin {
-        const val Version = "1.6.21"
+        const val Version = "1.7.0"
     }
 }
---
+++ src/main/kotlin/io/github/asheept/KotlinPlugin.kt
@@ -0,0 +1,11 @@
+package io.github.asheept
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class KotlinPlugin : JavaPlugin() {
+
+    override fun onEnable() {
+
+        println("hello")
+    }
+}
